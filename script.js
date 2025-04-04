445b91c9891743d86ed47b53bb31bee6dd90413e
bembee <bembee@freemail.hu>
2025-04-04 20:23:30 +0200

    update

--- script.js
+++ script.js
@@ -1,58 +1,51 @@
-var tipps = [];
-var intervallumTime = 15; // seconds;
-var tippsNumber = 0;
-var isFullscreen = false;
-var elem = document.documentElement;
+(() => {
+  let tipps = [];
+  let last5tippsNumbers = [];
+  const intervallumTime = 15; // seconds
+  const lastxNumber = 5;
+  let tippsNumber = 0;
+  const elem = document.documentElement;
 
-$(document).ready(function () {
-  $.getJSON("hints.json", function (data) {
-    $.each(data, function (key, value) {
-      tipps[key] = value.label;
+  $(document).ready(function () {
+    $.getJSON("hints.json", function (data) {
+      $.each(data, function (key, value) {
+        tipps[key] = value.label;
+      });
+      setTips(generateRandomTipp());
     });
-    setTips(generateRandomTipp());
+    setInterval(function () {
+      setTips(generateRandomTipp());
+    }, intervallumTime * 1000);
   });
-  setInterval(function () {
-    setTips(generateRandomTipp());
-  }, intervallumTime * 1000);
-});
 
-function setTips(tippNumber) {
-  $(tipp).text(tipps[tippNumber]);
-}
-
-function generateRandomTipp() {
-  tippsNumber = Math.floor(Math.random() * tipps.length);
-  if (tippsNumber >= tipps.length - 1) {
-    tippsNumber = 0;
+  function setTips(tippNumber) {
+    $(tipp).text(tipps[tippNumber]);
   }
-  return tippsNumber;
-}
 
-function openFullscreen() {
-  console.log(isFullscreen);
-  if (!isFullscreen) {
-    isFullscreen = true;
-    $("#fullscreenButton").html("Exit Fullscreen");
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) {
-      /* Safari */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) {
-      /* IE11 */
-      elem.msRequestFullscreen();
-    }
-  } else {
-    isFullscreen = false;
-    $("#fullscreenButton").html("Fullscreen");
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      /* Safari */
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-      /* IE11 */
-      document.msExitFullscreen();
-    }
+  function generateRandomTipp() {
+    do {
+      tippsNumber = generateRandomNumber();
+    } while (last5tippsNumbers.includes(tippsNumber));
+
+    last5tippsNumbers.push(tippsNumber);
+    if (last5tippsNumbers.length > lastxNumber) last5tippsNumbers.shift();
+
+    return tippsNumber;
   }
-}
+
+  function generateRandomNumber() {
+    return Math.floor(Math.random() * tipps.length);
+  }
+
+  window.toggleFullScreen = function () {
+    if (document.fullscreenElement) {
+      document.exitFullscreen?.() ||
+        document.webkitExitFullscreen?.() ||
+        document.msExitFullscreen?.();
+    } else {
+      elem.requestFullscreen?.() ||
+        elem.webkitRequestFullscreen?.() ||
+        elem.msRequestFullscreen?.();
+    }
+  };
+})();
